3b0add7cab8f89db759b8e03bb06f571a9ed50b6
Nanosabre <nanosabre@gmail.com>
2023-06-30 00:06:03 -0400

    Added up to loss function and optimizer

--- PyTorchWorkflow1.py
+++ PyTorchWorkflow1.py
@@ -14,8 +14,8 @@ print("Running on Device: " + device)
 torch.manual_seed(RANDOMSEED)
 
 # Linear Regression with known parameters
-intercept = 0.7
-slope = 0.3
+intercept = 0.3
+slope = 0.7
 
 start = 0
 end = 1
@@ -47,5 +47,33 @@ def plotPredictions(trainData=X_train,
     #Legend
     plt.legend(prop={"size":14})
     plt.show()
+class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch inherits from nn.Module
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.bias = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+    #Forward Method, defines computation
 
-plotPredictions()
+    def forward(self, x: torch.Tensor) -> torch.Tensor: # "x" is the input data
+        return self.weights * x + self.bias
+
+
+
+model0 = LinearRegressionModel()
+
+# Loss Function
+lossFn = nn.L1Loss()
+# Optimizer
+optimizer = torch.optim.SGD(params=model0.parameters(), lr=0.01)
+
+#print(list(model0.parameters()))
+
+## Predictions using torch.inference_mode
+with torch.inference_mode():
+    y_preds = model0(X_test)
+
+
+
+print(y_preds)
+
+plotPredictions(predictions=y_preds)

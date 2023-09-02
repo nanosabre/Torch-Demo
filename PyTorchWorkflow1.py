3dc973451d4a20c64fa96ddfc567724abfab96f2
Nanosabre <nanosabre@gmail.com>
2023-09-02 10:35:14 -0400

    small changes

--- PyTorchWorkflow1.py
+++ PyTorchWorkflow1.py
@@ -4,7 +4,7 @@ from torch import nn
 #import numpy as np
 import matplotlib.pyplot as plt
 
-RANDOMSEED = 42
+RANDOMSEED = 40
 
 # Version info
 print("Torch Version: " + str(torch.__version__))
@@ -21,7 +21,7 @@ start = 0
 end = 1
 step = 0.02
 X = torch.arange(start, end, step).unsqueeze(dim=1)
-y = slope * X + intercept
+y = slope * X * X + intercept
 
 # Establish training and testing sets
 trainSplit = int(0.8*len(X))
@@ -50,12 +50,13 @@ def plotPredictions(trainData=X_train,
 class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch inherits from nn.Module
     def __init__(self):
         super().__init__()
-        self.weights = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
-        self.bias = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.p1 = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.p2 = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.p3 = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
     #Forward Method, defines computation
 
     def forward(self, x: torch.Tensor) -> torch.Tensor: # "x" is the input data
-        return self.weights * x + self.bias
+        return self.p1 * x * x + self.p2 * x + self.p3
 
 
 
@@ -68,12 +69,56 @@ optimizer = torch.optim.SGD(params=model0.parameters(), lr=0.01)
 
 #print(list(model0.parameters()))
 
-## Predictions using torch.inference_mode
+## Predictions using torch.inference_mode, FORWARD PASS
+#with torch.inference_mode():
+#    y_preds = model0(X_test)
+
+torch.manual_seed(RANDOMSEED)
+
+X_train = X_train.to(device)
+X_test = X_test.to(device)
+y_train = y_train.to(device)
+y_test = y_test.to(device)
+model0.to(device)
+
+lossplot = []
+
+epochs = 1000
+#Loops Through Data
+for epoch in range(epochs):
+    # Set model to training mode
+    model0.train() #Train mode in Pytorch sets all parameters that require gradients to require gradients
+    
+    # 1. Forward Pass
+    y_preds = model0(X_train)
+
+    # 2. Loss Function
+    loss = lossFn(y_preds, y_train)
+    lossplot.append(loss.cpu().item())
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Perform backprop on the loss with respect to parameters
+    loss.backward()
+
+    # 5. Step the optimizer
+    optimizer.step() # By default how the optimizer changes will accumulate through the loop, so they have to be zerod in step 3
+
+
+    model0.eval() # turn of gradient tracking
+
+    with torch.inference_mode():
+        test_pred = model0(X_test)
+        test_loss = lossFn(test_pred, y_test)
+    
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Train loss: {loss} | Test loss: {test_loss}")
+
+model0.eval()
 with torch.inference_mode():
-    y_preds = model0(X_test)
-
-
-
-print(y_preds)
-
-plotPredictions(predictions=y_preds)
+    y_pred = model0(X_test)
+print(model0.state_dict())
+plt.scatter(torch.arange(0, len(lossplot), 1).unsqueeze(dim=1), lossplot, s=2)
+plt.show()
+plotPredictions(predictions=y_pred.cpu())
